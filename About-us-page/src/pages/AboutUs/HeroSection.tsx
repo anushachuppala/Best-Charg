70779f8dc8e8529eaaf69dc4104707cd9312e320
anushachuppala <anusha.chuppala12@gmail.com>
2026-07-23 22:48:11 +0530

    completed the hero section

--- About-us-page/src/pages/AboutUs/HeroSection.tsx
+++ About-us-page/src/pages/AboutUs/HeroSection.tsx
@@ -1,42 +1,44 @@
 import heroImage from "../../assets/images/About-page/hero-section.png";
 import styles from "./HeroSection.module.css";
-import NavBar from "../../widgets/NavBar";
+import { Container } from "../../shared/layout/container";
 
 function HeroSection() {
   return (
-    <section className={styles.herosection}>
+    <section className={styles.heroSection}>
       <img
         src={heroImage}
         alt="EV Charging Infrastructure"
-        className={styles.heroimage}
+        className={styles.heroImage}
       />
 
-      <div className={styles.herooverlay}></div>
+      <div className={styles.heroOverlay}></div>
 
-      <div className={styles.herocontent}>
-        <p className={styles.smallheading}>ABOUT BEST CHARG</p>
+      <Container className={styles.heroContainer}>
+        <div className={styles.heroContent}>
+          <p className={styles.smallHeading}>ABOUT BEST CHARG</p>
 
-        <h1>
-          Engineering the Future
-          <br />
-          of <span>EV Charging</span>
-          <br />
-          Infrastructure
-        </h1>
+          <h1 className={styles.heroTitle}>
+            Engineering the Future
+            <br />
+            of <span>EV Charging</span>
+            <br />
+            Infrastructure
+          </h1>
 
-        <p className={styles.herodescription}>
-          Best Charg delivers intelligent EV charging infrastructure,
-          enterprise-grade software, and integrated mobility ecosystems —
-          helping businesses, governments, and communities transition
-          confidently toward cleaner transportation.
-        </p>
+          <p className={styles.heroDescription}>
+            Best Charg delivers intelligent EV charging infrastructure,
+            enterprise-grade software, and integrated mobility ecosystems —
+            helping businesses, governments, and communities transition
+            confidently toward cleaner transportation.
+          </p>
 
-        <div className={styles.herobuttons}>
-          <button className={styles.explorebtn}>Explore Solutions</button>
+          <div className={styles.heroButtons}>
+            <button className={styles.exploreBtn}>Explore Solutions</button>
 
-          <button className={styles.expertsbtn}>Talk to Our Experts</button>
+            <button className={styles.expertsBtn}>Talk to Our Experts</button>
+          </div>
         </div>
-      </div>
+      </Container>
     </section>
   );
 }
