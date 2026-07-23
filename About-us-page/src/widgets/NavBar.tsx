6d620f3a55056a421a47366c08666a0b31a22de5
anushachuppala <anusha.chuppala12@gmail.com>
2026-07-23 15:54:50 +0530

    completed the NavBar

--- About-us-page/src/widgets/NavBar.tsx
+++ About-us-page/src/widgets/NavBar.tsx
@@ -1,26 +1,24 @@
-import "./NavBar.modules.css";
+import styles from "./NavBar.module.css";
 import logo from "../assets/images/logo.png";
 import { IoChevronDown } from "react-icons/io5";
 
 function NavBar() {
   return (
-    <nav className="navbar">
-      <div className="logo-container">
-        <img src={logo} alt="Best Charg Logo" className="logo-image" />
+    <nav className={styles.navbar}>
+      <div className={styles.logocontainer}>
+        <img src={logo} alt="Best Charg Logo" className={styles.logoimage} />
       </div>
-
       <ul>
         <li className="products-menu">
           Products
-          <IoChevronDown className="dropdown-icon" />
+          <IoChevronDown className={styles.icon} />
         </li>
         <li>Solutions</li>
         <li>Best Hub</li>
         <li>Blog</li>
         <li>About us</li>
       </ul>
-
-      <button className="Btn-get-started">Get Started</button>
+      <button className={styles["Btn-get-started"]}>Get Started</button>
     </nav>
   );
 }
